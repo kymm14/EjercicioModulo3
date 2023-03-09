f082a4ef1e8732cb4cf4350844647cbb18f63112
LucianoEpifani <85083543+lucianoEpifani@users.noreply.github.com>
2023-03-09 10:30:23 +0100

    Módulo-3 Java

--- Ejerciciomodulo3.java
+++ Ejerciciomodulo3.java
@@ -1,48 +1,58 @@
 
+Ejercicio 1
+
 public class Ejerciciomodulo3 {
   public static void main(String[] args) {
     System.out.println("Buenos días");
-    System.out.println("8*13");
+    System.out.println("8*13");  
     int resultado = 8 * 13;
     System.out.println("El resultado de la multiplicación 8*13 es: " + resultado);
+    
+    
+    Ejercicio 2
+      
     int resultado2 = 1 + 2 + 3;
     System.out.println("1 + 2 + 3 = " + resultado2);
     int a = 2;
     int b = 3;
     int c = 4;
+    
+    Ejercicio 3
 
     System.out.println("La suma de las tres variables es: " + (a + b + c));
-
     c = 5;
-
     System.out.printf("La suma de las tres variables es: %d\n", a + b + c);
     
-    int angulo1 = 60;
-    int angulo2 = 60;
-    int angulo3 = 60;
+    Ejercicio 4
+    
+    int angulito1 = 60;
+    int angulito2 = 60;
+    int angulito3 = 60;
 
-    if (angulo1 + angulo2 + angulo3 == 180) {
+    if (angulito1 + angulito2 + angulito3 == 180) {
       System.out.println("Estos ángulos pueden formar un triángulo.");
     } else {
       System.out.println("Estos ángulos no pueden formar un triángulo.");
       
-      int num1 = 5;
-      int num2 = 10;
-      int num3 = 15;
-      int num4 = 20;
+      Ejercicio 5
+      
+      int numero1 = 5;
+      int numero2 = 10;
+      int numero3 = 15;
+      int numero4 = 20;
 
-      int mayor = num1;
+      int mayor = numero1;
 
-      if (num2 > mayor) {
-        mayor = num2;
+      if (numero2 > mayor) {
+        mayor = numero2;
       }
 
-      if (num3 > mayor) {
-        mayor = num3;
+      if (numero3 > mayor) {
+        mayor = numero3;
       }
 
-      if (num4 > mayor) {
-        mayor = num4;
+      if (numero4 > mayor) {
+        mayor = numero4;
       }
 
       System.out.println("El número mayor es: " + mayor);
